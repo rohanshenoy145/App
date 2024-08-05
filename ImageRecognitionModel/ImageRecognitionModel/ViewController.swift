bed7f681300820357bc38554eef765441324336d
Rohan Shenoy <rohanshenoy145@gmail.com>
2024-08-05 16:43:10 -0400

    Add files via upload

---
+++ ImageRecognitionModel/ImageRecognitionModel/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ImageRecognitionModel
+//
+//  Created by Rohan Shenoy on 6/27/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
